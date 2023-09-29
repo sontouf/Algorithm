0dc16a53184eb58e427ad1e4ba766ce6d505784e
sontouf <61100126+sontouf@users.noreply.github.com>
2023-09-29 19:25:24 +0900

    [Silver III] Title: N과 M (3), Time: 2100 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+def dfs(path):
+    if len(path) == M:
+        print(*path)
+        return
+    
+    for i in range(1, N+1):
+        dfs([*path, i])
+
+
+if __name__ == '__main__':
+    N, M = map(int, input().split())
+
+    dfs([])
