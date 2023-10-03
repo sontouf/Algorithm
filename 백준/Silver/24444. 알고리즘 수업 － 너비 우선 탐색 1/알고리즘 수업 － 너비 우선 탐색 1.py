04308c330794d7be6b4c545d3c45298f30fdcbcb
sontouf <61100126+sontouf@users.noreply.github.com>
2023-10-04 03:16:47 +0900

    Update 알고리즘 수업 － 너비 우선 탐색 1.py

--- 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.py
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.py
@@ -1,31 +1,30 @@
-from collections import deque
 import sys
+from collections import deque
 input = sys.stdin.readline
 
-n, m, r = map(int, input().split())
-graph = [[] for _ in range(n + 1)]
-visited = [0] * (n + 1)
-count = 1
-
-for _ in range(m):
-    a, b = map(int, input().split())
-    graph[a].append(b)
-    graph[b].append(a)
-
-def bfs(v):
-    global count
-
-    q = deque([r])
+def bfs(r):
+    queue = deque()
     visited[r] = 1
-    while q:
-        v = q.popleft()
-        graph[v].sort()
-        for g in graph[v]:
-            if visited[g] == 0:
+    count = 1
+    queue.append(r)
+    while queue:
+        cur = queue.popleft()
+        graph[cur].sort()
+        for nxt in graph[cur]:
+            if visited[nxt] == 0:
                 count += 1
-                visited[g] = count
-                q.append(g)
-bfs(r)
+                visited[nxt] = count
+                queue.append(nxt)
 
-for v in visited[1:]:
-    print(v)
+
+if __name__=='__main__':
+    N, M, R = map(int ,input().split())
+    graph = [[] for _ in range(N+1)]
+    visited = [0] * (N+1)
+    for _ in range(M):
+        u, v = map(int ,input().split())
+        graph[u].append(v)
+        graph[v].append(u)
+    bfs(R)
+    for i in range(1, N+1):
+        print(visited[i])
