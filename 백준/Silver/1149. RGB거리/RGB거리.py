0a94c058aa7b9b73e9d2a29572868ee263545df4
sontouf <61100126+sontouf@users.noreply.github.com>
2023-10-30 12:59:42 +0900

    [Silver I] Title: RGB거리, Time: 120 ms, Memory: 110404 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.py
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -1,12 +1,11 @@
-n = int(input())
-a = [0]*n
+import sys
+input = sys.stdin.readline
 
-for i in range(n):
-    a[i] = list(map(int,input().split()))
-    
-for i in range(1,n): # 1부터 시작하는 이유는 다음 입력값이 이전 입력값의 최소값을 사용하기때문이다
-    a[i][0]= min(a[i-1][1],a[i-1][2]) + a[i][0]
-    a[i][1]= min(a[i-1][0],a[i-1][2]) + a[i][1]
-    a[i][2]= min(a[i-1][0],a[i-1][1]) + a[i][2]
-
-print(min(a[n-1][0],a[n-1][1],a[n-1][2]))
+if __name__=='__main__':
+	N = int(input())
+	color = [list(map(int, input().split())) for _ in range(N)]
+	
+	for i in range(1, N):
+		for j in range(3):
+			color[i][j] = min(color[i-1][(j+1)%3], color[i-1][(j+2)%3]) + color[i][j]
+	print(min(color[N-1][0], color[N-1][1], color[N-1][2]))
