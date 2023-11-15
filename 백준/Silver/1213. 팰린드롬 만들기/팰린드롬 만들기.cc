a5c0557909ac9ce358839775da030d8d8b09e364
sontouf <61100126+sontouf@users.noreply.github.com>
2023-11-15 16:42:25 +0900

    [Silver III] Title: 팰린드롬 만들기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.cc
+++ 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.cc
@@ -2,38 +2,30 @@
 using namespace std;
 
 int arr[26];
-string s, temp1, temp2;
+string s, temp1;
 int main() {
 	cin >> s;
 	int oddcnt = 0;
 	int onlyodd = -1;
-	for (int i = 0; i < s.size(); i++) {
-		arr[s[i]- 'A']++;
-	}
-	for (int i = 0; i < 26; i++) {
-		if (arr[i] % 2 == 1) {
+	for (char a : s) arr[a-'A']++;
+	for (int i = 25; i >= 0; i--) {
+		if (arr[i] & 1) {
 			oddcnt++;
 			if (oddcnt > 1) {
 				cout << "I'm Sorry Hansoo" << '\n';
 				exit(0);
 			}
 			onlyodd = i;
-			for (int j = 0; j < (arr[i] - 1) / 2; j++) {
-				temp1 += (char)(i + 'A');
-			}
+			arr[i]--;
 		}
-		else {
-			for (int j = 0; j < arr[i] / 2; j++) {
-				temp1 += (char)(i + 'A');
-			}
+		for (int j = 0; j < arr[i]; j += 2) {
+			temp1 = (char)(i + 'A') + temp1;
+			temp1 += (char)(i + 'A');
 		}
 	}
-	temp2 = temp1;
 	if (onlyodd != -1)
-		temp2 += (char)(onlyodd + 'A');
-	reverse(temp1.begin(), temp1.end());
-	temp2 += temp1;
-	cout << temp2 << '\n';
+		temp1.insert(temp1.begin() + (temp1.size() / 2), onlyodd + 'A');
+	cout << temp1 << '\n';
 
 	return 0;
 }
